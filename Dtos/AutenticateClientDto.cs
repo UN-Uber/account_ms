6b2b47e5a548756d600d62109d3631c5cd8ec411
OscarAlejandroGomez <ogomezs@unal.edu.co>
2021-12-12 19:14:50 -0500

    Endpoint para la autenticación, correccion de constrains en la base de dastos y algunos errores, en rama deploy

--- Dtos/AutenticateClientDto.cs
+++ Dtos/AutenticateClientDto.cs
@@ -3,7 +3,7 @@ namespace account_ms.Dtos
     public class AtenticateClientDto
     {
         public string email {get;set;}
-        public string telNumber {get;set;}
+        public long telNumber {get;set;}
         public string password {get; set;}
     }
 }
--- Dtos/AutenticateClientResponse.cs
+++ Dtos/AutenticateClientResponse.cs
@@ -2,6 +2,6 @@ namespace account_ms.Dtos
 {
     public class AutenticateClientResponse
     {
-        public String repuesta {get;set;}
+        public string response { get; set;}
     }
 }
